44cf848deb08b0cb83757d72606c9ab191fa93a2
thomasmorin-insee <thomas.morin@insee.fr>
2025-11-13 08:28:51 +0000

    init historique

---
+++ historique-par-communes.R
@@ -0,0 +1,71 @@
+install.packages(c("ohsome"))
+
+library(ohsome)
+library(osmdata)
+library(sf)
+library(dplyr)
+library(aws.s3)
+library(arrow)
+library(httr)
+library(jsonlite)
+
+
+# Départements OSM
+liste_dep_osm <- c(
+  "01" = "Ain", "02" = "Aisne", "03" = "Allier", "04" = "Alpes-de-Haute-Provence", "05" = "Hautes-Alpes", "06" = "Alpes-Maritimes", "07" = "Ardèche", "08" = "Ardennes",
+  "09" = "Ariège", "10" = "Aube", "11" = "Aude", "12" = "Aveyron", "13" = "Bouches-du-Rhône", "14" = "Calvados", "15" = "Cantal", "16" = "Charente", "17" = "Charente-Maritime",
+  "18" = "Cher", "19" = "Corrèze", "2A" = "Corse-du-Sud", "2B" = "Haute-Corse", "21" = "Côte-d'Or", "22" = "Côtes-d'Armor", "23" = "Creuse", "24" = "Dordogne",
+  "25" = "Doubs", "26" = "Drôme", "27" = "Eure", "28" = "Eure-et-Loir", "29" = "Finistère", "30" = "Gard", "31" = "Haute-Garonne", "32" = "Gers", "33" = "Gironde", "34" = "Hérault",
+  "35" = "Ille-et-Vilaine", "36" = "Indre", "37" = "Indre-et-Loire", "38" = "Isère", "39" = "Jura", "40" = "Landes", "41" = "Loir-et-Cher", "42" = "Loire",
+  "43" = "Haute-Loire", "44" = "Loire-Atlantique", "45" = "Loiret", "46" = "Lot", "47" = "Lot-et-Garonne", "48" = "Lozère", "49" = "Maine-et-Loire", "50" = "Manche",
+  "51" = "Marne", "52" = "Haute-Marne", "53" = "Mayenne", "54" = "Meurthe-et-Moselle", "55" = "Meuse", "56" = "Morbihan", "57" = "Moselle", "58" = "Nièvre", "59" = "Nord",
+  "60" = "Oise", "61" = "Orne", "62" = "Pas-de-Calais", "63" = "Puy-de-Dôme", "64" = "Pyrénées-Atlantiques", "65" = "Hautes-Pyrénées", "66" = "Pyrénées-Orientales",
+  "67" = "Bas-Rhin", "68" = "Haut-Rhin", "69" = "Rhône", "70" = "Haute-Saône", "71" = "Saône-et-Loire", "72" = "Sarthe", "73" = "Savoie", "74" = "Haute-Savoie",
+  "75" = "Paris", "76" = "Seine-Maritime", "77" = "Seine-et-Marne", "78" = "Yvelines", "79" = "Deux-Sèvres", "80" = "Somme", "81" = "Tarn", "82" = "Tarn-et-Garonne",
+  "83" = "Var", "84" = "Vaucluse", "85" = "Vendée", "86" = "Vienne", "87" = "Haute-Vienne", "88" = "Vosges", "89" = "Yonne", "90" = "Territoire de Belfort", "91" = "Essonne",
+  "92" = "Hauts-de-Seine", "93" = "Seine-Saint-Denis", "94" = "Val-de-Marne", "95" = "Val-d'Oise",
+  "971" = "Guadeloupe", "972" = "Martinique", "973" = "Guyane",
+  "974" = "La Réunion", "976" = "Mayotte"
+)
+
+code_dep <- "13"
+code_commune <- "13001"
+
+# Récupère les objets "commune" dans le département "Nom-du-dépt, France"
+communes_osm <- paste0(liste_dep_osm[code_dep], ", France") %>%
+  opq() %>%
+  add_osm_feature(key = "boundary", value = "administrative") %>%
+  add_osm_feature(key = "admin_level", value = "8") %>%
+  osmdata_sf()
+
+# On ne garde que les communes dans le département
+communes_sf <- communes_osm$osm_multipolygons %>% 
+  filter(substr(`ref:INSEE`,1,2) == !!code_dep)
+
+# Polygone de la commune
+poly_commune <- communes_sf %>% filter(`ref:INSEE` == code_commune)
+# plot(poly_commune)
+
+# Convertir le polygone en GeoJSON (format attendu par l'API ohsome)
+poly_geojson <- sf_geojson(poly_commune)
+
+# Construire le corps de la requête
+body <- list(
+  bpolys = poly_geojson,
+  filter = "highway=* and not motorway=* and not trunk=*", # Exemple : toutes les routes sauf autoroutes et voies principales
+  time = "2024-02-01", # Date souhaitée
+  showMetadata = FALSE # Optionnel : pour ne pas inclure les métadonnées
+)
+
+# Envoyer la requête POST
+r <- POST(
+  url = "https://api.ohsome.org/v1/elements/geometry",
+  body = body,
+  encode = "json"
+)
+
+# Vérifier le statut de la réponse
+status_code(r)
+
+# Extraire et traiter les données GeoJSON retournées
+result <- content(r, "text", encoding = "UTF-8")
