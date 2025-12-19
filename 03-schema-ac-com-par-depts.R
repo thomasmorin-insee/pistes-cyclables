4c56a0d9cabd5cceb14e70c5347d953d9860cd51
thomasmorin-insee <thomas.morin@insee.fr>
2025-12-19 14:53:27 +0000

    réorganisation

--- 03-schema-ac-com-par-depts.R
+++ 03-schema-ac-com-par-depts.R
@@ -17,32 +17,30 @@ library(glue)
 BUCKET <- "zg6dup"
 
 # Input/output
-annee <- 2025
+annee <- 2017
 input_file <- "at36vc/api-ohsome-com-par-depts-{annee}/lg-com-dep{code_dep}.parquet"
 output_file <- "at36vc/schema-ac-com-par-depts-{annee}/dept-{code_dep}.parquet"
-# input_file <- "schema-ac-com-par-depts-01-06-2024/lines-com-dep{code_dep}.parquet"
-# output_file <- "schema-ac-com-par-depts-01-06-2024/schema-ac-dept-{code_dep}-bis.parquet"
 
 # liste des tags utiles 
-file_tags <- "./pistes-cyclables/_tags-osm.R"
+file_tags <- "_tags-osm.R"
 liste_tags <- eval(parse(text = paste(readLines(file_tags), collapse = "\n")))
 
 # Requête sql
-file_sql <- "./pistes-cyclables/_schema-amenagements-cyclables.sql"
+file_sql <- "_schema-amenagements-cyclables.sql"
 requete_sql <- readChar(file_sql, file.info(file_sql)$size)
 
 liste_dep <- c(
-  # "01", "02", "03", "04", "05", "06", "07", "08", "09", 
-  # "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
-  # "2A", "2B", "21", "22", "23", "24", "25", "26", "27", "28", "29",
-  # "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
-  # "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
-  # "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
-  # "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
-  # "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
-  # "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
-  # "90", "91", "92", "93", "94", "95", 
-  "971", "972", "973", "974", "976"
+  "01", "02", "03", "04", "05", "06", "07", "08", "09",
+  "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+  "2A", "2B", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+  "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+  "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+  "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+  "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+  "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+  "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+  "90", "91", "92", "93", "94", "95",
+  "971", "972", "973", "974"
 )
 
 # Boucle sur les départements
